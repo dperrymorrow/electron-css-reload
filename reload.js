9b198dcffa729a95e2d988ac2e61d0019378a64a
dperrymorrow@gmail.com <david.morrow@DMORROW-MBP.local>
2016-09-12 12:18:59 -0700

    using fs.watch instead of interval and stat
    this makes it much more efficient, only updates
    on change from the file system

--- reload.js
+++ reload.js
@@ -2,38 +2,17 @@
 const fs = require('fs');
 const CACHE_PARAM = "?nocache=";
 
-function _refresh() {
+module.exports = function() {
   let sheets = Array.from(document.querySelectorAll('link[rel="stylesheet"]'));
 
   sheets.forEach(sheet => {
-
-    fs.stat(_cleanUrl(sheet.href), (err, data) => {
-      if (!err) {
-        let modTime = new Date(data.mtime).getTime();
-        let lastUpdated = _lastUpdated(sheet.href);
-
-        if (!lastUpdated || lastUpdated < modTime) {
-          _reloadSheet(sheet);
-        }
-      }
+    fs.watchFile(_nodePath(sheet.href), (curr, pref) => {
+      sheet.href = sheet.href.split(CACHE_PARAM)[0] + CACHE_PARAM + new Date().getTime();
+      console.log(`reloading: ${sheet.href}`);
     });
-
   });
 }
 
-function _lastUpdated(url) {
-  return url.split(CACHE_PARAM)[1];
-}
-
-function _reloadSheet(sheet) {
-  sheet.href = sheet.href.split(CACHE_PARAM)[0] + CACHE_PARAM + new Date().getTime();
-  console.log(`reloading: ${sheet.href}`);
-}
-
-function _cleanUrl(url) {
+function _nodePath(url) {
   return url.replace('file://', '').split(CACHE_PARAM)[0];
 }
-
-module.exports = function () {
-  setInterval(_refresh, 3000);
-};
