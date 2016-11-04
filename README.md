d6e04f3677a39ac563f7fcbf670cd0522480a403
David Morrow <david.morrow@DMORROW-MBP.local>
2016-11-04 14:36:35 -0700

    watching inline styles,

--- README.md
+++ README.md
@@ -24,9 +24,35 @@ require('electron-css-reload')();
 
 That't it, enjoy not restarting your Electron app every time you wanna see a CSS change.
 
+## Also watching dependencies of the css file
+
+You can also watch dependencies of the CSS file you have loaded. This is helpful for situations like including other CSS files into your main file.
+
+
+```html
+<link rel="stylesheet" type="text/css" href="../../frontend/styles/index.css" data-source="fonts.css,animations.css"/>
+```
+
+> the paths in data-sources should be relative to the file path of the href
+
+## Watching inline-styles
+
+You can also watch inline styles for changes by giving an id, and a ```data-source```.
+
+
+> data-source path should be top down from your electron application.
+
+```html
+<style type="text/css" id="test-id" data-source="/app/pages/main/index.html">
+  body {
+    background-color: red;
+  }
+</style>
+```
+
 ## How does it work?
 
-Since Electron has access to the file system, it uses ```fs.watchFile``` to watch for changes to the files loaded via ```<link>``` tags on the page. 
+Since Electron has access to the file system, it uses ```fs.watchFile``` to watch for changes to the files loaded via ```<link>``` tags on the page.
 
 When a change event fires, a ```?nocache={timestamp}``` is added to the href on the stylesheet link on the page.
-This forces the browser to reload the sheet as it sees it as a new url.
+This forces the browser to reload the sheet as it sees it as a new URL.
--- reload.js
+++ reload.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 const CACHE_PARAM = "?nocache=";
+const opts = {
+  persistant: true,
+  interval: 1000,
+};
+
+let stash = document.createElement("div");
+let htmlFiles = [];
+let watchedFiles = [];
 
 module.exports = function() {
+  _inlineStyles();
+  setInterval(_externalSheets, opts.interval);
+}
+
+function _externalSheets() {
   let sheets = Array.from(document.querySelectorAll('link[rel="stylesheet"]'));
 
   sheets.forEach(sheet => {
-    var opts = {
-      persistant: true,
-      interval: 1000,
-    };
 
     let mainFile = _nodePath(sheet.href);
-    let deps = sheet.getAttribute('deps');
+    let deps = sheet.getAttribute('data-sources');
     let prefix = mainFile.split('/');
 
     prefix.splice(prefix.length - 1, 1)
@@ -21,14 +30,57 @@ module.exports = function() {
     deps = deps.map((dep) => `${prefix}/${dep}`);
     deps.push(mainFile);
 
-    deps.forEach((dep) => {
-      console.log(`watching ${dep} for changes`);
-      fs.watchFile(dep, opts, (curr, pref) => {
-        sheet.href = sheet.href.split(CACHE_PARAM)[0] + CACHE_PARAM + new Date().getTime();
-        console.log(`reloading: ${sheet.href}`);
+    try {
+      deps.forEach((dep) => {
+        console.log(`watching ${dep} for changes`);
+        fs.watchFile(dep, opts, (curr, pref) => {
+          sheet.href = sheet.href.split(CACHE_PARAM)[0] + CACHE_PARAM + new Date().getTime();
+          console.log(`reloading: ${sheet.href}`);
+        });
       });
-    });
+    } catch (err) {
+      _throw(err);
+    }
   });
+}
+
+function _inlineStyles() {
+  let styles = Array.from(document.getElementsByTagName('style'));
+
+  styles.forEach(style => {
+    let file = fs.realpathSync(process.cwd() + style.getAttribute('data-source'));
+    if (!htmlFiles.includes(file)) htmlFiles.push(file);
+  });
+
+  htmlFiles.forEach(file => {
+    try {
+      console.log(`watching ${file} for changes`);
+
+      if (watchedFiles.includes(file)) return;
+      watchedFiles.push(file);
+
+      fs.watchFile(file, opts, (curr, pref) => {
+        fs.readFile(file, 'utf-8', (err, data) => {
+          if (err) _throw(err);
+          stash.innerHTML = data;
+          let updated = Array.from(stash.getElementsByTagName('style'));
+          updated.forEach(sheet => {
+            let id = sheet.getAttribute('id');
+            document.getElementById(id).innerHTML = sheet.innerHTML;
+          })
+        });
+      });
+
+    } catch (err) {
+      _throw(err);
+    }
+  });
+
+
+}
+
+function _throw(err) {
+  console.warn('electron-css-reload:', err.message, err.stack)
 }
 
 function _nodePath(url) {
